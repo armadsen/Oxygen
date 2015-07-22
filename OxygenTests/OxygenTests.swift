b9d590bb61243135adb48989af4a0dea70844974
Andrew Madsen <andrew@openreelsoftware.com>
2015-07-21 21:13:30 -0600

    Initial commit.

---
+++ OxygenTests/OxygenTests.swift
@@ -0,0 +1,36 @@
+//
+//  OxygenTests.swift
+//  OxygenTests
+//
+//  Created by Andrew Madsen on 7/21/15.
+//  Copyright (c) 2015 Open Reel Software. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class OxygenTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ Sources/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Oxygen
+//
+//  Created by Andrew Madsen on 7/21/15.
+//  Copyright (c) 2015 Open Reel Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+

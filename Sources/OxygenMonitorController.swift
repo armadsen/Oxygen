b4dfb3e0f584f093d4f833a883a86935791b8e54
Andrew Madsen <andrew@openreelsoftware.com>
2015-07-21 21:16:20 -0600

    Added ORSSerialPort as submodule and linking it into app.

---
+++ Sources/OxygenMonitorController.swift
@@ -0,0 +1,14 @@
+//
+//  OxygenMonitorController.swift
+//  Oxygen
+//
+//  Created by Andrew Madsen on 7/21/15.
+//  Copyright (c) 2015 Open Reel Software. All rights reserved.
+//
+
+import Foundation
+import ORSSerial
+
+class OxygenMonitorController: NSObject {
+   
+}
